1328d6a5107391b5ade2f44197cf28e79862a271
珅 <sazs@live.cn>
2020-09-01 00:21:24 +0800

    lxk0301.js

--- lxk0301.js
+++ lxk0301.js
@@ -10,30 +10,31 @@ const { parse } = require("path");
 // 公共变量
 const JD_COOKIE = process.env.JD_COOKIE; //cokie,多个用&隔开即可
 const SyncUrl = process.env.SYNCURL; //签到地址,方便随时变动
-const PUSH_KEY = process.env.PUSH_KEY; //推送消息地址
+const PUSH_KEY = process.env.PUSH_KEY; //server酱推送消息
+const BARK_PUSH = process.env.BARK_KEY; //Bark推送
 const JDMarketCoinToBeans = process.env.JDMarketCoinToBeans; //京小超蓝币兑换京豆数量
 const JDJoyFeedCount = process.env.JDJoyFeedCount; //宠汪汪喂食数量
+const FruitShareCodes = process.env.FruitShareCodes; //京东农场分享码
 let CookieJDs = [];
 
 async function downFile() {
     await download(SyncUrl, "./", { filename: "temp.js" });
 }
+
 async function downNotifyFile() {
     await download("https://github.com/lxk0301/scripts/raw/master/sendNotify.js", "./", { filename: "sendNotify.js" });
+}
+
+async function downFruitShareCodesFile() {
+    await download("https://github.com/lxk0301/scripts/raw/master/jdFruitShareCodes.js", "./", { filename: "jdFruitShareCodes.js" });
 }
 
 async function changeFiele() {
     let content = await fs.readFileSync("./temp.js", "utf8");
     content = content.replace("require('./jdCookie.js')", JSON.stringify(CookieJDs));
-    if (!PUSH_KEY) {
-        content = content.replace("require('./sendNotify')", "''");
-    }
-    if (JDMarketCoinToBeans &&!isNaN(JDMarketCoinToBeans)&& parseInt(JDMarketCoinToBeans) <= 20 && parseInt(JDMarketCoinToBeans) >= 0) {
-        content = content.replace("$.getdata('coinToBeans')", JDMarketCoinToBeans);
-    }
-    if (JDJoyFeedCount && !isNaN(JDJoyFeedCount) && [10, 20, 40, 80].indexOf(parseInt(JDJoyFeedCount) >= 0)) {
-        content = content.replace("$.getdata('joyFeedCount')", JDJoyFeedCount);
-    }
+    if (!PUSH_KEY && !BARK_KEY) content = content.replace("require('./sendNotify')", "''");
+    if (JDMarketCoinToBeans &&!isNaN(JDMarketCoinToBeans)&& parseInt(JDMarketCoinToBeans) <= 20 && parseInt(JDMarketCoinToBeans) >= 0) content = content.replace("$.getdata('coinToBeans')", JDMarketCoinToBeans);
+    if (JDJoyFeedCount && !isNaN(JDJoyFeedCount) && [10, 20, 40, 80].indexOf(parseInt(JDJoyFeedCount) >= 0)) content = content.replace("$.getdata('joyFeedCount')", JDJoyFeedCount);
 
     await fs.writeFileSync("./lxk0301.js", content, "utf8");
 }
@@ -52,11 +53,13 @@ async function start() {
     // 下载最新代码
     await downFile();
     console.log("下载代码完毕");
-    if (PUSH_KEY) {
+    if (PUSH_KEY || BARK_KEY) {
         await downNotifyFile();
         console.log("下载通知代码完毕");
-    } else {
-        console.log("无PUSH_KEY,不发送微信通知");
+    }
+    if (FruitShareCodes) {
+        await downFruitShareCodesFile();
+        console.log("下载农场分享码代码完毕");
     }
     await changeFiele();
     console.log("替换变量完毕");
