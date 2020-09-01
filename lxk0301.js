7e7a0b722ed35367d2290b6fab87d6f18a5a1286
珅 <ys06000@ly.com>
2020-09-01 11:42:27 +0800

    喂食速度降低

--- lxk0301.js
+++ lxk0301.js
@@ -1,61 +1,78 @@
 const exec = require("child_process").execSync;
 const fs = require("fs");
 const download = require("download");
-const { parse } = require("path");
+// const { parse } = require("path");
 
 // 公共变量
-const JD_COOKIE = process.env.JD_COOKIE; //cokie,多个用&隔开即可
-const SyncUrl = process.env.SYNCURL; //签到地址,方便随时变动
-const PUSH_KEY = process.env.PUSH_KEY; //server酱推送消息
-const BARK_PUSH = process.env.BARK_KEY; //Bark推送
-const JDMarketCoinToBeans = process.env.JDMarketCoinToBeans; //京小超蓝币兑换京豆数量
-const JDJoyFeedCount = process.env.JDJoyFeedCount; //宠汪汪喂食数量
-const FruitShareCodes = process.env.FruitShareCodes; //京东农场分享码
+const Secrets = {
+    JD_COOKIE: process.env.JD_COOKIE, //cokie,多个用&隔开即可
+    SyncUrl: process.env.SYNCURL, //签到地址,方便随时变动
+    PUSH_KEY: process.env.PUSH_KEY, //server酱推送消息
+    BARK_KEY: process.env.BARK_KEY, //Bark推送
+    MarketCoinToBeanCount: process.env.JDMarketCoinToBeans, //京小超蓝币兑换京豆数量
+    JoyFeedCount: process.env.JDJoyFeedCount, //宠汪汪喂食数量
+    FruitShareCodes: process.env.FruitShareCodes, //京东农场分享码
+};
 let CookieJDs = [];
 
 async function downFile() {
-    await download(SyncUrl, "./", { filename: "temp.js" });
+    await download(Secrets.SyncUrl, "./", { filename: "temp.js" });
     console.log("下载代码完毕");
-    if (PUSH_KEY || BARK_KEY) {
-        await download("https://github.com/lxk0301/scripts/raw/master/sendNotify.js", "./", { filename: "sendNotify.js" });
+    if (Secrets.PUSH_KEY || Secrets.BARK_KEY) {
+        await download("https://github.com/lxk0301/scripts/raw/master/sendNotify.js", "./", {
+            filename: "sendNotify.js",
+        });
         console.log("下载通知代码完毕");
     }
-    if (FruitShareCodes) {
-        await download("https://github.com/lxk0301/scripts/raw/master/jdFruitShareCodes.js", "./", { filename: "jdFruitShareCodes.js" });
+    if (Secrets.FruitShareCodes) {
+        await download("https://github.com/lxk0301/scripts/raw/master/jdFruitShareCodes.js", "./", {
+            filename: "jdFruitShareCodes.js",
+        });
         console.log("下载农场分享码代码完毕");
     }
 }
 
 async function changeFiele() {
     let content = await fs.readFileSync("./temp.js", "utf8");
-    
+
     content = content.replace("require('./jdCookie.js')", JSON.stringify(CookieJDs));
-    
-    if (!PUSH_KEY && !BARK_KEY) content = content.replace("require('./sendNotify')", "''");
-    
-    if (JDMarketCoinToBeans &&!isNaN(JDMarketCoinToBeans)&& parseInt(JDMarketCoinToBeans) <= 20 && parseInt(JDMarketCoinToBeans) >= 0) content = content.replace("$.getdata('coinToBeans')", JDMarketCoinToBeans);
-    
-    if (JDJoyFeedCount && !isNaN(JDJoyFeedCount) && [10, 20, 40, 80].indexOf(parseInt(JDJoyFeedCount) >= 0)) content = content.replace("$.getdata('joyFeedCount')", JDJoyFeedCount);
+
+    if (!Secrets.PUSH_KEY && !Secrets.BARK_KEY) content = content.replace("require('./sendNotify')", "''");
+
+    if (
+        Secrets.MarketCoinToBeanCount &&
+        !isNaN(Secrets.MarketCoinToBeanCount) &&
+        parseInt(Secrets.MarketCoinToBeanCount) <= 20 &&
+        parseInt(Secrets.MarketCoinToBeanCount) >= 0
+    )
+        content = content.replace("$.getdata('coinToBeans')", Secrets.MarketCoinToBeanCount);
+
+    if (
+        Secrets.JoyFeedCount &&
+        !isNaN(Secrets.JoyFeedCount) &&
+        [10, 20, 40, 80].indexOf(parseInt(Secrets.JoyFeedCount) >= 0)
+    )
+        content = content.replace("$.getdata('joyFeedCount')", Secrets.JoyFeedCount);
 
     await fs.writeFileSync("./lxk0301.js", content, "utf8");
     console.log("替换变量完毕");
 }
 
 async function start() {
-    if (!JD_COOKIE) {
+    if (!Secrets.JD_COOKIE) {
         console.log("请填写 JD_COOKIE 后在继续");
         return;
     }
-    if (!SyncUrl) {
+    if (!Secrets.SyncUrl) {
         console.log("请填写 SYNCURL 后在继续");
         return;
     }
     CookieJDs = JD_COOKIE.split("&");
+    console.log(Secrets);
     console.log(`当前共${CookieJDs.length}个账号需要签到`);
-    // 下载最新代码
-    await downFile();
-    await changeFiele();
     try {
+        await downFile();
+        await changeFiele();
         await exec("node lxk0301.js", { stdio: "inherit" });
     } catch (e) {
         console.log("执行异常:" + e);
