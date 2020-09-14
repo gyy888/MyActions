96cafb5190bf21cdd065c4b6b46ff7c211f5f21a
珅 <sazs@live.cn>
2020-09-15 00:51:23 +0800

    取关

--- smartReplace.js
+++ smartReplace.js
@@ -45,6 +45,17 @@ async function replaceWithSecrets(content, Secrets) {
             replacements.push({ key: "$.getdata('joyFeedCount')", value: feedCount });
         }
     }
+    if (Secrets.Unsubscribe) {
+        if(Secrets.Unsubscribe.splite(',').length != 4) {
+            console.log('取关参数不正确，请参考readme中的提示填入，记得用英文逗号,隔开');
+        } else {
+            let usinfo = Secrets.Unsubscribe.splite(',');
+            replacements.push({ key: "$.getdata('jdUnsubscribePageSize')", value: isNaN(usinfo[0]) ? 0 : usinfo[0] });
+            replacements.push({ key: "$.getdata('jdUnsubscribeShopPageSize')", value: isNaN(usinfo[1]) ? 50 : usinfo[1] });
+            replacements.push({ key: "$.getdata('jdUnsubscribeStopGoods')", value: $"'{usinfo[2]}'"  });
+            replacements.push({ key: "$.getdata('jdUnsubscribeStopShop')", value: $"'{usinfo[3]}'" });
+        }
+    }
     return batchReplace(content, replacements);
 }
 function batchReplace(content, replacements) {
