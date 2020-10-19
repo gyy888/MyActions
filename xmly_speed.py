0a4b5321ab5624e704e2e41dfdf3dab459dc1fa1
珅 <ys06000@ly.com>
2020-10-19 21:41:22 +0800

    ~

--- xmly_speed.py
+++ xmly_speed.py
@@ -28,8 +28,15 @@ def run():
             if len(rewriteAgent) ==0 or rewriteAgent.strip()=='':
                 rewriteAgent='UserAgent = "ting_1.8.30(Redmi+7,Android28)"'
                 executeContent = re.sub(agentPattern,rewriteAgent,executeContent)
-        if xmly_accumulate_index.index(idx)>=0:
-            executeContent = executeContent.replace("XMLY_ACCUMULATE_TIME = 1","XMLY_ACCUMULATE_TIME = 0",1).replace("action 自动刷时长打开","action 自动刷时长被指定关闭",1)
+        if len(xmly_accumulate_index)>0:
+            index = -1
+            try:
+                index = xmly_accumulate_index.index(idx)
+            except ValueError:
+                print("无需禁用")
+            if index>=0:
+                executeContent = executeContent.replace("XMLY_ACCUMULATE_TIME = 1","XMLY_ACCUMULATE_TIME = 0",1).replace("action 自动刷时长打开","action 自动刷时长被指定关闭",1)
+                print("已接受XMLY_ACCUMULATE_INDEX配置,为索引"+idx+"的数据执行了禁用刷新时长的操作")
         writeFile(executeContent,'execute'+str(idx)+'.py')
         os.system('python ./'+'execute'+str(idx)+'.py')
     print("\n***************************\n文件全部执行完毕")
